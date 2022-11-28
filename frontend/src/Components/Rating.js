1dceeafc717b7d3e067f8f1b74e186a3951e46b8
SUNIL7978 <Sunilprusty16@gmail.com>
2022-11-28 20:46:39 +0530

    Create Product and Rating Component

--- frontend/src/Components/Rating.js
+++ frontend/src/Components/Rating.js
@@ -5,19 +5,19 @@ import { BiStar } from 'react-icons/bi'
 const Rating = ({stars,numReviews }) => {
     return (
         <div className='rating'>
-            <span className='star_yellow'>
+            <span>
                 {stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BiStar />}
             </span>
-            <span className='star_yellow'>
+            <span>
                 {stars >= 2 ? <BsStarFill /> : stars >= 1.5 ? <BsStarHalf /> : <BiStar />}
             </span>
-            <span className='star_yellow'>
+            <span>
                 {stars >= 3 ? <BsStarFill /> : stars >= 2.5 ? <BsStarHalf /> : <BiStar />}
             </span>
-            <span className='star_yellow'>
+            <span >
                 {stars >= 4 ? <BsStarFill /> : stars >= 3.5 ? <BsStarHalf /> : <BiStar />}
             </span>
-            <span className='star_yellow'>
+            <span>
                 {stars >= 5 ? <BsStarFill /> : stars >= 4.5 ? <BsStarHalf /> : <BiStar />}
             </span>
         </div>
