9ae890f0a5a385316a6f8e0c6a774c9d41f9aeca
SUNIL7978 <Sunilprusty16@gmail.com>
2022-12-01 11:19:29 +0530

    Create Signin Backend API

--- README.md
+++ README.md
@@ -111,3 +111,9 @@
 
     1. create user model
     2. seed sample users
+
+18.Create Signin Backend API
+
+    1. create signin api
+    2. npm install jsonwebtoken
+    3. define generateToken
--- backend/Server.js
+++ backend/Server.js
@@ -3,19 +3,24 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import seedRouter from './Routes/seedRoutes.js';
 import productRouter from './Routes/productRoutes.js';
+import userRouter from './Routes/userRoutes.js';
 
 dotenv.config()
 
-mongoose.connect(process.env.MANGODB_LOCAL).then(() =>{
+mongoose.connect(process.env.MANGODB_LOCAL).then(() => {
   console.log('connect to db')
-}).catch((err)=>{
+}).catch((err) => {
   console.log(err.message)
 })
 
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
-app.use('/api/products' , productRouter)
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
 // app.get('/api/products', (req, res) => {
 //   res.send(data.products)
@@ -38,6 +43,10 @@ app.use('/api/products' , productRouter)
 //     res.status(404).send({ message: 'Product Not Found' });
 //   }
 // });
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
